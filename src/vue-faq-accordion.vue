2bc2238e26a48d4ddf2f1afc166bb721c99d205c
sandiprb <baradiya.sandip@gmail.com>
2019-12-19 04:14:40 +0530

    Allow option to render as markdown

--- src/vue-faq-accordion.vue
+++ src/vue-faq-accordion.vue
@@ -25,7 +25,11 @@
             <collapse-transition>
               <div v-if="i === activeQuestionIndex">
                 <slot v-bind:item="item">
-                  <p class="accordion__value" v-html="item[answerProperty]"></p>
+                  <p class="accordion__value" v-if="renderAs === 'markdown'">
+                    <vue-markdown>{{ item[answerProperty] }}</vue-markdown>
+                  </p>
+                  <p class="accordion__value" v-else-if="renderAs === 'text'">{{ item[answerProperty] }}</p>
+                  <p class="accordion__value" v-else v-html="item[answerProperty]"></p>
                 </slot>
               </div>
             </collapse-transition>
@@ -38,6 +42,7 @@
 
 <script>
   import { CollapseTransition } from 'vue2-transitions'
+  import VueMarkdown from 'vue-markdown'
 
   export default {
     name: 'VueFaqAccordion',
@@ -103,7 +108,15 @@
       fontColor: {
         type: String,
         default: '#000000'
-      }
+      },
+      /**
+       * various render type options 
+       * possible values: 'text', 'html', 'markdown'
+       */
+      renderAs: {
+        type: String,
+        default:'html',
+      },
     },
     computed: {
       categories () {
